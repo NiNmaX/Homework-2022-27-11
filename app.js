0c0deb667a2bde8c1a51928f3f2784e923af7c5a
Abdusamadova Nigina <nigina2007abdusamadova@gmail.com>
2022-11-29 18:42:23 +0500

    homework-2022-27-11

---
+++ app.js
@@ -0,0 +1,36 @@
+let bodyEl = document.querySelector("#output-color");
+let box1El = document.querySelector(".box-1")
+
+box1El.addEventListener("click", function() {
+  bodyEl.style.backgroundColor = "aqua";
+});
+
+let box2El = document.querySelector(".box-2")
+
+box2El.addEventListener("click", function() {
+  bodyEl.style.backgroundColor = "red";
+});
+
+let box3El = document.querySelector(".box-3")
+
+box3El.addEventListener("click", function() {
+  bodyEl.style.backgroundColor = "green";
+});
+
+let box4El = document.querySelector(".box-4")
+
+box4El.addEventListener("click", function() {
+  bodyEl.style.backgroundColor = "yellow";
+});
+
+let box5El = document.querySelector(".box-5")
+
+box5El.addEventListener("click", function() {
+  bodyEl.style.backgroundColor = "pink";
+});
+
+let box6El = document.querySelector(".box-6")
+
+box6El.addEventListener("click", function() {
+  bodyEl.style.backgroundColor = "brown";
+});
